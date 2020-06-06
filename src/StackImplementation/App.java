60c27a7fec486a3bb95155c650022771d911c516
sidji <sidji@sidji-Latitude-E6410>
2020-06-06 21:42:29 +0530

    Static method for reversing string through Stack

--- src/StackImplementation/App.java
+++ src/StackImplementation/App.java
@@ -3,23 +3,34 @@ package StackImplementation;
 public class App {
 
 	public static void main(String[] args) {
-			Stack st = new Stack(10);
-			st.push(20);
-			st.push(40);
-			st.push(60);
-			st.push(80);
-			st.push(100);
-			st.push(120); 
-			st.push(140);
-		 
+		Stack st = new Stack(10); 
+		/*
+		 * st.push(20); st.push(40); st.push(60); st.push(80);
+		 * st.push(100); st.push(120); st.push(140);
+		 * 
+		 * 
+		 * 
+		 * while(!st.isEmpty()) { System.out.println(st.pop()); }
+		 * System.out.println("--------------------------");
+		 * 
+		 * System.out.println(st.pop());
+		 */
+		 System.out.println(reverseString("siddharth"));
 			
-		
-		  while(!st.isEmpty()) { System.out.println(st.pop()); } 
-		  System.out.println("--------------------------"); 
-		 
-		  System.out.println(st.pop());
-		 
-			
+	}
+	public static String reverseString(String str) {
+		int stringLength = str.length();
+		Stack  stack = new Stack(stringLength);
+		for(int i =0; i< str.length(); i++) {
+			char ch = str.charAt(i);
+			stack.push(ch);
+		}
+		String result="";
+		while(!stack.isEmpty()) {
+			char ch = (char) stack.pop();
+			result = result + ch;
+		}
+		return result;
 	}
 
 }
